5594e7e1e6f07f779e4047cf0ede2f71f761e84d
IgorCh27 <svkchumak@gmail.com>
2025-03-02 16:58:15 +0200

    colapsed

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
         <AppTitle title = {<h2>This is app component</h2>} />
         Arcticle 1
         <Rating value = {4} />
-        <Accordion titleApp = {<h3>Title-Menu</h3>}/>
+        <Accordion titleApp = {<h3>Title-Menu</h3>} colapsed = {true} />
         <AppTitle title = {<h2>Friends container</h2>} />
         Arcticle 2
         <Rating value = {3} />
-        <Accordion titleApp = {<h3>Page stories</h3>}/>
+        <Accordion titleApp = {<h3>Page stories</h3>} colapsed = {false} />
     </div>
   );
 }
--- src/components/Accordion/Acordion.jsx
+++ src/components/Accordion/Acordion.jsx
@@ -1,10 +1,18 @@
 import { AccordionTitle } from "./AccordionTittle"
 import { AccordionBody } from "./AccordionBody"
 export function Accordion(props) {
+  if(props.colapsed) {
     return(
-    <div>  
-    <AccordionTitle title = {props.titleApp}/>
-    <AccordionBody />
-    </div>
-    )
+      <div>  
+      <AccordionTitle title = {props.titleApp}/>
+      <AccordionBody />
+      </div>
+      )
+  } else {
+    return(
+      <div>  
+      <AccordionTitle title = {props.titleApp}/>
+      </div>
+      )
+  }
   }
