ae244b8587ec3e534de3fe2246bda0a68d3fe5a4
IgorCh27 <svkchumak@gmail.com>
2025-03-02 19:45:43 +0200

    rendering

--- src/components/Accordion/Acordion.jsx
+++ src/components/Accordion/Acordion.jsx
@@ -3,18 +3,10 @@ import { AccordionTitle } from "./AccordionTittle"
 import { AccordionBody } from "./AccordionBody"
 
 export function Accordion(props) {
-  if(props.colapsed) {
     return(
       <div>  
       <AccordionTitle title = {props.titleApp}/>
+      {!props.colapsed && <AccordionBody />}
       </div>
       )
-  } else {
-    return(
-      <div>  
-      <AccordionTitle title = {props.titleApp}/>
-      <AccordionBody />
-      </div>
-      )
-  }
   }
